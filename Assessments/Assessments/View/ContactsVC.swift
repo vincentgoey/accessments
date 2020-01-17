6e7c146340fe3a4e87022d6fa7da7580621ec450
Vincent Goey <vincent.goey1003@gmail.com>
2020-01-18 01:22:22 +0800

    populate data from json file and display as scene 1

--- Assessments/Assessments/View/ContactsVC.swift
+++ Assessments/Assessments/View/ContactsVC.swift
@@ -9,9 +9,103 @@
 import UIKit
 
 class ContactsVC: UIViewController {
+    
+    var tableView = UITableView()
+    private var contactViewModels = ContactViewModel()
+    
+    struct Cells {
+        static let contactCell = "ContactCell"
+    }
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action:#selector(handleRefresh(_:)), for: UIControl.Event.valueChanged)
+        refreshControl.attributedTitle = NSAttributedString(string: "Updating") //if you want a text
+        
+        return refreshControl
+    }()
+    
+    @objc func handleRefresh(_ refreshControl: UIRefreshControl) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            self.contactViewModels.retrieveData(contacts: self.fetchData())
+            self.tableView.reloadData()
+            refreshControl.endRefreshing()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupData()
+        setupNavBar()
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        
+        setTableViewDelegate()
+        tableView.rowHeight = 80
+        tableView.register(ContactCell.self, forCellReuseIdentifier: Cells.contactCell)
+        tableView.tableFooterView = UIView()
+        tableView.refreshControl = refreshControl
+        tableView.pin(to: view)
+        
+    }
+    
+    func setTableViewDelegate() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func setupNavBar() {
+        self.title = "Contacts"
+        
+        //setup Right Navigation Button
+        let imageIcon = UIImage(named: appConstant.addIcon)
+        let rightButton = UIBarButtonItem(image: imageIcon,  style: .plain, target: self, action:#selector(addNewContact))
+        rightButton.tintColor = hexStringToUIColor(hex: appConstant.themeColor)
+        self.navigationItem.rightBarButtonItem = rightButton
+    }
+    
+    func setupData() {
+        self.contactViewModels.retrieveData(contacts: fetchData())
     }
 
+}
+
+extension ContactsVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.contactViewModels.numberOfRows(section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cells.contactCell) as! ContactCell
+        cell.contactModel = self.contactViewModels.modelAt(indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ContactsVC {
+    
+    func fetchData() -> [Contact]  {
+        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let contactlists = try JSONDecoder().decode([Contact].self, from: data)
+                return contactlists
+            } catch {
+                // handle error
+            }
+        }
+        return []
+    }
+    
+    @objc func addNewContact(){
+        print("Tapped")
+    }
+    
 }
--- Assessments/Assessments/ViewModel/ContactViewModel.swift
+++ Assessments/Assessments/ViewModel/ContactViewModel.swift
@@ -11,5 +11,22 @@ import Foundation
 struct ContactViewModel {
 
     private var contactsList = [Contact]()
+    
+    mutating func retrieveData(contacts: [Contact]) {
+        //Clear Old data
+        self.contactsList.removeAll()
+        //Add new data
+        contacts.forEach { (contact) in
+            self.contactsList.append(contact)
+        }
+    }
+    
+    func numberOfRows(_ section: Int) -> Int {
+        return self.contactsList.count
+    }
+    
+    func modelAt(_ index: Int) -> Contact {
+        return self.contactsList[index]
+    }
 
 }
