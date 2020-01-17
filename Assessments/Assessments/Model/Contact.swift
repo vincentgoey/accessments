e8b0e420714bbd0d529079710b9b4d8a4b51d432
Vincent Goey <vincent.goey1003@gmail.com>
2020-01-17 23:57:55 +0800

    implement contact model, viewmodel and added json file

--- Assessments/Assessments/Model/Contact.swift
+++ Assessments/Assessments/Model/Contact.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+
+struct Contact: Codable {
+    
+    var id          :String?
+    var firstName   :String
+    var lastName    :String
+    var email       :String?
+    var phone       :String?
+    
+}
