7d07c69d619c156c16d93fec40ed3f435d36ff43
Vincent Goey <vincent.goey1003@gmail.com>
2020-01-17 23:31:34 +0800

    init project

---
+++ Assessments/Assessments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assessments
+//
+//  Created by Kai Xuan on 17/01/2020.
+//  Copyright © 2020 Kai Xuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
